b4a93bcc6cfc4f11ebe3520028c8d021fe143205
Kiet Duong Hung <kietdh@ssg.vn>
2023-03-09 23:31:47 +0700

    Add project files.

---
+++ Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? LoginName { get; set; }
+        public string? SapCode { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
